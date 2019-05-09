9138d8fb03c968c53c8c8f3adc27cf83d27f77e5
Cristian Pop <cristian.pop@sectorlabs.ro>
2019-05-09 15:29:38 +0300

    update to 64.2

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,14 +1,14 @@
 class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://ssl.icu-project.org/"
-  url "https://ssl.icu-project.org/files/icu4c/63.1/icu4c-63_1-src.tgz"
-  mirror "https://github.com/unicode-org/icu/releases/download/release-63-1/icu4c-63_1-src.tgz"
-  version "63.1"
-  sha256 "05c490b69454fce5860b7e8e2821231674af0a11d7ef2febea9a32512998cb9d"
+  url "https://ssl.icu-project.org/files/icu4c/64.2/icu4c-64_2-src.tgz"
+  mirror "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
+  version "64.2"
+  sha256 "627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
 
   bottle do
     cellar :any
-    root_url "https://homebrew.bintray.com/bottles/"
+    root_url "https://homebrew.bintray.com/bottles/"    
     sha256 "e707bf5e3d0189ede7d941d95a417b5dacad3eac99b9a677042464140f12fa1d" => :mojave
     sha256 "0ac5ee60393d26ec26a915ed957a38a0b2355fe7991f607044edaedd3ff14cc1" => :high_sierra
     sha256 "dd64c70639ba91fe0c07304c0563681ba97b221e9f9919497b383884b8bb0b65" => :sierra
